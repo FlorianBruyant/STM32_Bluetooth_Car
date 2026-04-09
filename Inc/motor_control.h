8647304950532b016cb56d488b9b1139277736cf
Florian <bruyantflorian2@gmail.com>
2026-04-09 22:24:49 +0200

    base addresses

---
+++ Inc/motor_control.h
@@ -0,0 +1,11 @@
+/**
+ * @file      motor_control.h
+ * @brief     Header file for motor_control.c
+ */
+
+#ifndef MOTOR_CONTROL_H_
+#define MOTOR_CONTROL_H_
+
+
+
+#endif /* MOTOR_CONTROL_H_ */
---
+++ Src/motor_control.c
@@ -0,0 +1,27 @@
+/**
+ * @file      motor_control.c
+ * @brief     Implementation of DC motors controls
+ */
+
+#include "motor_control.h"
+#include <stdint.h>
+
+/* Base addresses */
+
+// Base address of the clock control block
+#define RCC_BASE 0x40023800U
+
+// Clock register to turn GPIO on or off
+#define RCC_AHB1ENR (*(volatile uint32_t *)(RCC_BASE + 0x30U))
+
+// Base addresses for GPIO A and B ports
+#define GPIOA_BASE 0x40020000U
+#define GPIOB_BASE 0x40020400U
+
+// GPIOA MODER and BSSR
+#define GPIOA_MODER   (*(volatile uint32_t *)(GPIOA_BASE + 0x00U))
+#define GPIOA_BSRR    (*(volatile uint32_t *)(GPIOA_BASE + 0x18U))
+
+// GPIOB MODER and BSSR
+#define GPIOB_MODER   (*(volatile uint32_t *)(GPIOB_BASE + 0x00U))
+#define GPIOB_BSRR    (*(volatile uint32_t *)(GPIOB_BASE + 0x18U))
