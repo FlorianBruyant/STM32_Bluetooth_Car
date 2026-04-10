286f94d93d49744a6313c52e65f8ae929c13273d
Florian <bruyantflorian2@gmail.com>
2026-04-10 16:53:32 +0200

    doc: GPIO_Init

--- Drivers/Device/Inc/gpio.h
+++ Drivers/Device/Inc/gpio.h
@@ -10,6 +10,15 @@
 
 #include "CMSIS/stm32f411xe.h"
 
+/**
+ * @brief  Initializes the GPIOx peripheral according to the specified parameters.
+ * @param  port:       Pointer to GPIOx peripheral base address (e.g., GPIOA, GPIOB).
+ * @param  pin:        Specifies the port pin to be configured (0 to 15).
+ * @param  mode:       Operating mode for the selected pin (Input, Output, Alternate, Analog).
+ * @param  outputType: Operating output type for the selected pin (Push-pull or Open-drain).
+ * @param  outputSpeed: Specifies the speed for the selected pin (Low, Medium, Fast, High).
+ * @param  pullUpDown: Specifies the Pull-up or Pull-down activation for the selected pin.
+ */
 void GPIO_Init(GPIO_TypeDef* port,
 		uint8_t pin,
 		uint8_t mode,
